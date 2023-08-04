84f02d8da5aa872740e08abb1ccd906ad3c85660
Egor <egor29302@yandex.ru>
2023-08-04 16:27:42 +0300

    first review

---
+++ yacut/views.py
@@ -0,0 +1,46 @@
+import random
+import string
+
+from flask import abort, flash, redirect, render_template, request
+
+from . import app, db
+from .forms import URLForm
+from .models import URLMap
+from .utils import get_unique_short
+
+
+# def get_unique_short_id(length):
+#     return ''.join(
+#         random.choice(
+#             string.ascii_letters + string.digits
+#         ) for _ in range(length)
+#     )
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index_view():
+    form = URLForm()
+    if form.validate_on_submit():
+        custom_id = form.custom_id.data
+        if not custom_id:
+            custom_id = get_unique_short()
+        elif URLMap.query.filter_by(short=custom_id).first():
+            form.custom_id.errors = [f'Имя {custom_id} уже занято!']
+            return render_template('index.html', form=form)
+        url_map = URLMap(
+            original=form.original_link.data,
+            short=custom_id,
+        )
+        db.session.add(url_map)
+        db.session.commit()
+        flash(f'Ваша новая ссылка готова: '
+              f'<a href="{request.base_url}{custom_id}">'
+              f'{request.base_url}{custom_id}</a>')
+    return render_template('index.html', form=form)
+
+
+@app.route('/<string:short>', methods=['GET'])
+def yacat_redirect(short):
+    print(short)
+    return redirect(
+        URLMap.query.filter_by(short=short).first_or_404().original)
